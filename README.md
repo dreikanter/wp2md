3f3536487395f3d62c2cec7213e0c475bbf10b4d
Alex Musayev <alex.musayev@gmail.com>
2012-07-09 16:32:52 +0400

    Dump path generation.

--- README.md
+++ README.md
@@ -1,25 +1,29 @@
 # Wordpress to Markdown Exporter
 
-A python script to convert Wordpress XML dump to a set of plain text/markdown files.
+A python script to convert Wordpress XML dump to a set of plain text/[markdown](http://daringfireball.net/projects/markdown) files. Intended to be used for migration from Wordpress to [public-static](http://github.com/dreikanter/public-static) website generator but could also be helpful as general purpose content processor.
 
 Requirements:
+
 * Python 2.7
 * Python-Markdown
 
 ## Processing
 
 Common blog metadata:
+
 * Collect rss/channel element data
 * Save it in key:value format to 'blog.txt'
 
 Posts and pages:
+
 * Collect rss/channel/item elements data where post_type is 'page' or 'post'
 * Store the data in separate file for each post
 * File names should be '{date:YYYY.MM.DD}_{post_id}_{post_name}.txt'
 * Posts and pages should be kept in 'posts' and 'pages' directories respectively.
-* Comments
+* Comments [TBD]
 
 Images:
+
 * Extract all src values from img-elements inside posts
 * Extend all relative URLs to absolute using root URL from RSS header
 * Map URL to new image names: {post date}_{post title}_{#}.{ext}
@@ -27,20 +31,22 @@ Images:
 * Replace src attribute values for all img elements during .md files generation
 
 Attachments:
+
 * Collect all relative and ansolute URLs from a-elements poining to the site
 * Do the same thing as for images
 
 Comments file format:
 
-[comment header]
-[comment body]
----
-[next comment header]
-[next comment body]
---
-...
+	[comment header]
+	[comment body]
+	--
+	[next comment header]
+	[next comment body]
+	--
+	...
 
 Comment header includes all exported fields:
+
 * id
 * author
 * author_email
--- wp2md.py
+++ wp2md.py
@@ -3,6 +3,7 @@
 
 import os.path
 import codecs
+import time
 import logging
 import traceback
 from xml.etree.ElementTree import XMLParser
@@ -12,18 +13,13 @@ from xml.etree.ElementTree import XMLParser
 WHAT2SAVE = {
     'channel': [
         'title',
-        'link',
         'description',
-        'pubDate',
-        'language',
-        'base_site_url',
-        'base_blog_url',
-        'author_id',
+        'author_display_name',
         'author_login',
         'author_email',
-        'author_display_name',
-        'author_first_name',
-        'author_last_name',
+        'base_site_url',
+        'base_blog_url',
+        'language',
     ],
     'item': [
         'title',
@@ -71,8 +67,10 @@ def init_conf():
     # TODO: Get it from args
     global conf
     conf['source_file'] = 'wordpress.xml'
-    conf['dump_path'] = 'out'
+    conf['dump_path'] = None
     conf['verbose'] = True
+    conf['parse_date_fmt'] = "%a, %d %b %Y %H:%M:%S +0000"
+    conf['date_fmt'] = "%Y/%m/%d %H:%M:%S"
 
 
 def init_logging(verbose=False):
@@ -98,40 +96,64 @@ def tag_name(name):
     return name[name.find('}') + 1:]
 
 
-def dumpdata(file_name, data):
+def dump(file_name, data):
     """Dumps a dictionary to YAML-like text file."""
-    dir_path = os.path.dirname(os.path.abspath(file_name))
-    if dir_path and not os.path.exists(dir_path):
-        os.makedirs(dir_path)
-    with codecs.open(file_name, 'w', 'utf-8') as f:
-        content = None
-        for field in data:
-            if field == 'content':
-                content = data[field]
-            else:
-                f.write(u"%s: %s\n" % (unicode(field), unicode(data[field])))
-        if content:
-            f.write('\n' + content)
+    try:
+        dir_path = os.path.dirname(os.path.abspath(file_name))
+        if dir_path and not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        with codecs.open(file_name, 'w', 'utf-8') as f:
+            content = None
+            for field in data:
+                if field == 'content':
+                    content = data[field]
+                else:
+                    f.write(u"%s: %s\n" % (unicode(field), unicode(data[field])))
+            if content:
+                f.write('\n' + content)
+    except Exception as e:
+        log.error("Error saving data to '%s'" % file_name)
+        log.debug(e)
 
 
-def get_dump_path(file_name):
-    return os.path.join(conf['dump_path'], os.path.basename(file_name))
+def get_dump_path(file_name, subdir=''):
+    """Generates dump directory absolute path."""
+    explicit = conf['dump_path']
+    result = explicit or '{date}_{source}'
+    result = result.format(date=time.strftime('%Y-%m-%d'),
+                           source=os.path.basename(conf['source_file']))
+    return os.path.join(os.path.abspath(result), subdir, file_name)
+
+
+def parse_date(date_str, default=None):
+    """Parses date string according to parse_date_fmt configuration parameter.
+    Returns parsed value in date_fmt format or default if parsing was filed."""
+    try:
+        result = time.strptime(date_str, conf['parse_date_fmt'])
+    except:
+        msg = "Error parsing date string '%s'. Using default value." % date_str
+        log.debug(msg)
+        result = default
+
+    return time.strftime(conf['date_fmt'], result) if result else ''
 
 
 def dump_channel(data):
-    # TODO: ...
-    dumpdata(get_dump_path('blog.txt'), data)
+    file_name = get_dump_path('blog.txt')
+    log.info("Dumping channel data to '%s'..." % file_name)
+
+    fields = WHAT2SAVE['channel']
+    processed = {field: data.get(field, None) for field in fields}
+
+    pub_date = data.get('pubDate', None)
+    processed['export_date'] = parse_date(pub_date, time.gmtime())
+
+    dump(file_name, processed)
 
 
 def dump_item(data):
+    log.info("Dumping item...")
     # TODO: ...
-    dumpdata(get_dump_path(''), data)
-    dump_comments(data)
-
-
-def dump_comments(data):
-    # TODO: ...
-    dumpdata(get_dump_path(''), data)
 
 
 class CustomParser:
